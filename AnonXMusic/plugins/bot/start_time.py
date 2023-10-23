04a85492c9cebab2a016bdad6841de7f75374113
xd <85302664+okaybroxd@users.noreply.github.com>
2023-10-23 11:14:44 +0530

    Update start_time.py

--- AnonXMusic/plugins/bot/start_time.py
+++ AnonXMusic/plugins/bot/start_time.py
@@ -1,1 +1,34 @@
+import time
 
+start_time = time.time()
+
+import psutil
+import time
+from _ import start_time, Client # replace _ where you declare the start_time, Client
+from pyrogram import filters 
+from pyrogram.types import Message
+
+# TeamUltroid/Ultroid
+def time_formatter(milliseconds):
+    minutes, seconds = divmod(int(milliseconds / 1000), 60)
+    hours, minutes = divmod(minutes, 60)
+    days, hours = divmod(hours, 24)
+    weeks, days = divmod(days, 7)
+    tmp = (((str(weeks) + "w:") if weeks else "") +
+           ((str(days) + "d:") if days else "") +
+           ((str(hours) + "h:") if hours else "") +
+           ((str(minutes) + "m:") if minutes else "") +
+           ((str(seconds) + "s") if seconds else ""))
+    if not tmp:
+        return "0s"
+    if tmp.endswith(":"):
+        return tmp[:-1]
+    return tmp
+
+
+@app.on_message(filters.command('statusbot') & filters.private)
+async def activevc(_, message: Message):
+    uptime = time_formatter((time.time() - start_time) * 1000)
+    cpu = psutil.cpu_percent()
+    TEXT = f"UPTIME: {uptime} | CPU: {cpu}%"
+    await message.reply(TEXT)
